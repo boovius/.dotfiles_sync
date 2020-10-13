341b1e135e4c7103155053e2f8f5453c4e2c655a
Joshua Book <joshua.book@gmail.com>
2020-10-13 13:26:15 -0700

    Update new mac setup for powerlevel 10k.

--- new_mac_setup.sh
+++ new_mac_setup.sh
@@ -36,18 +36,19 @@ brew cask install flycut
 brew cask install slack
 brew cask install sublime
 brew cask install ngrok
-brew cask install focused
 brew cask cleanup
 
 # ruby
 rbenv init
-rbenv install 2.3.0
-gem install bundler
+rbenv install 2.7.0
 gem install rails
 
 # node
 nvm install v8.2.1
 npm install -g mocha
+
+# zsh powerlevel 10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 # dotfiles
 git clone git@github.com:boovius/.dotfiles_sync.git ~/.dotfiles_sync
@@ -57,7 +58,7 @@ chmod +x makesymlinks.sh
 source ~/.bash_profile
 
 #vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #xcode
 install_mac_tools() {
